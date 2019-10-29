6204f2d83b5d0633ed7ab49140f1f11738a59fa9
rafael <barboza.rafael.p@gmail.com>
2019-10-28 20:46:22 -0500

    Create class Contributors and SearchDataResponse, whit the class Contributor added a new config Room

--- app/src/main/java/com/example/githubviewmodel/Models/POJOS.kt
+++ app/src/main/java/com/example/githubviewmodel/Models/POJOS.kt
@@ -1,8 +1,6 @@
 package com.example.githubviewmodel.Models
 
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.Index
+import androidx.room.*
 import com.google.gson.annotations.SerializedName
 
 @Entity(primaryKeys = ["login"])
@@ -12,3 +10,34 @@ data class User(val login:String, @SerializedName("avatar_url") val avatar:Strin
 @Entity(indices = [Index("id"), Index("owner_login")], primaryKeys = ["name", "owner_login"])
 data class Repos(@Embedded(prefix = "owner_")val owner:User, val id:Int, val name:String, val full_name:String,
                  val description:String, @SerializedName("stargazers_count")val start:Int)
+
+@Entity(foreignKeys = (
+    [ForeignKey(
+        entity = Repos::class,
+        parentColumns = ["name"],
+        childColumns = ["repo"],
+        onUpdate = ForeignKey.CASCADE
+    ),
+    ForeignKey(
+        entity = User::class,
+        parentColumns = ["owner"],
+        childColumns = ["login"],
+        onUpdate = ForeignKey.CASCADE
+    )
+    ]
+))
+data class Contributors(@SerializedName("login") @PrimaryKey val login: String,
+                        @SerializedName("contributions") val contributions: Int,
+                        val avatar_url:String,@PrimaryKey val repo:String,@PrimaryKey val owner:String)
+
+data class SearchDataResponse(@SerializedName("total_count")val count:Int,
+                              @SerializedName("Items")val repo:MutableList<Repos>,
+                              val nextPage:Int){
+
+    val listRespoId:MutableList<Int>
+        get() {
+            val repoId = mutableListOf<Int>()
+            repo.forEach { repoId.add(it.id) }
+            return repoId
+        }
+}
